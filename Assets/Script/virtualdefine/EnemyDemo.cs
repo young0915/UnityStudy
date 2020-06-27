23b60734c4cb882b4f4ab79fd6b3b5acb0560764
young0915 <38437784+young0915@users.noreply.github.com>
2020-06-28 03:50:46 +0900

    재정리

---
+++ Assets/Script/virtualdefine/EnemyDemo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyDemo : MonoBehaviour
+{
+    private void OnDisable()
+    {
+        WaterEnemy waterEnemy = new WaterEnemy();
+        waterEnemy.TakeDamge(10, Enemy.DamageType.Fire);
+        Debug.Log(waterEnemy.GetHitPoints());
+    }
+   
+}
---
+++ Assets/Script/virtualdefine/test.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+   //소스트리 적응을 위해  test했습니다. 이상없음
+}
