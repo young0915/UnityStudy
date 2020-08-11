460ee4585ba9879a2a39ab195c6b40a6c96f082b
young0915 <38437784+young0915@users.noreply.github.com>
2020-08-12 01:37:20 +0900

    Add files via upload

---
+++ Assets/Script/Generic/Singleton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    public static T instance;
+
+    protected virtual void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(this);
+            return;
+        }
+        instance = this as T;
+        DontDestroyOnLoad(this);
+    }
+}
