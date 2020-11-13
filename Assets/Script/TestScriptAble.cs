6aa5bd97cf0aad96cbe5c4f55e2d50edbb80c839
young0915 <38437784+young0915@users.noreply.github.com>
2020-11-13 22:43:59 +0900

    ScriptableObject 만드는 방법
    예시본

---
+++ Assets/Script/TestScriptAble.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+// ScriptableObject 만드는 방법.
+
+[CreateAssetMenu(fileName = "ScriptableObject", menuName = "ddd",  order = int.MaxValue )]
+[System.Serializable]
+public class TestScriptAble : ScriptableObject
+{
+    public int id;
+    public string strName;
+    public List<string> strDescript;
+    public bool IsDelicious;
+}
